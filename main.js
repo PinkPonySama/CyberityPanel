550a9955defd6a11742a1b420889a1e623134c01
PinkPonySama <105165195+PinkPonySama@users.noreply.github.com>
2022-11-10 20:22:25 +0300

    Добавил меню валидности

--- main.js
+++ main.js
@@ -35,16 +35,30 @@ function getDatePass() {
             userMonth -= 1;
             let getValidFullDate = new Date(getCardValidYear, userMonth, userDay);
             getValidFullDate.setDate(getValidFullDate.getDate() + 90);
-            let validDay = getValidFullDate.getDate();
-            let validMonth = getValidFullDate.getMonth();
+            var validDay = getValidFullDate.getDate();
+            var validMonth = getValidFullDate.getMonth();
             validMonth += 1;
             userMonth += 1;
             let validYear = getValidFullDate.getFullYear();
             let validFullDate = `${validDay}.${validMonth}.${validYear}`;
+            if (String(validDay).length < 2) {
+            	validDay = '0' + validDay
+            }
+            if (String(validMonth).length < 2) {
+            	validMonth = '0' + validMonth
+            }
 			if (nowMonth == userMonth && nowDay == userDay) {
-				alert(`Паспорт действителен\nc ${userDay}.${userMonth}.${getCardValibLastYear}\nдо ${validDay}.${validMonth}.${validYear}( ${validYear}-${validMonth}-${validDay} )\nДЕНЬ РОЖДЕНИЯ СЕГОДНЯ!\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userYear}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
+				document.getElementById("getCardValibLastYear").innerHTML="Годен с: " + userDay + '.' + userMonth + '.' + getCardValibLastYear;
+            	document.getElementById("getCardValidYear").innerHTML="Годен до: " + validDay + '.' + validMonth +'.' + validYear + '( ' + validYear + '-' + validMonth + '-' + validDay +' )';
+            	document.getElementById("user_age").innerHTML="Возраст: " + userAge;
+				document.getElementById("date_now").innerHTML="Дата сегодня: " + nowDay + '.' + nowMonth  + '.' + nowYear;
+				//alert(`Паспорт действителен\nc ${userDay}.${userMonth}.${getCardValibLastYear}\nдо ${validDay}.${validMonth}.${validYear}( ${validYear}-${validMonth}-${validDay} )\nДЕНЬ РОЖДЕНИЯ СЕГОДНЯ!\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userYear}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
 			} else {
-				alert(`Паспорт действителен\nc ${userDay}.${userMonth}.${getCardValibLastYear}\nдо ${validDay}.${validMonth}.${validYear}( ${validYear}-${validMonth}-${validDay} )\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userYear}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
+				document.getElementById("getCardValibLastYear").innerHTML="Годен с: " + userDay + '.' + userMonth + '.' + getCardValibLastYear;
+            	document.getElementById("getCardValidYear").innerHTML="Годен до: " + validDay + '.' + validMonth +'.' + validYear + '( ' + validYear + '-' + validMonth + '-' + validDay +' )';
+            	document.getElementById("user_age").innerHTML="Возраст: " + userAge;
+				document.getElementById("date_now").innerHTML="Дата сегодня: " + nowDay + '.' + nowMonth  + '.' + nowYear;
+				//alert(`Паспорт действителен\nc ${userDay}.${userMonth}.${getCardValibLastYear}\nдо ${validDay}.${validMonth}.${validYear}( ${validYear}-${validMonth}-${validDay} )\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userYear}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
 			}
 		} else if (userAge >= 20 && userAge < 45) {
 			// С этого годе исполнилось 20
@@ -54,23 +68,45 @@ function getDatePass() {
             userMonth -= 1;
             let getValidFullDate = new Date(getCardValidYear, userMonth, userDay);
             getValidFullDate.setDate(getValidFullDate.getDate() + 90);
-            let validDay = getValidFullDate.getDate();
-            let validMonth = getValidFullDate.getMonth();
+            var validDay = getValidFullDate.getDate();
+            var validMonth = getValidFullDate.getMonth();
             validMonth += 1;
             userMonth += 1;
             let validYear = getValidFullDate.getFullYear();
             let validFullDate = `${validDay}.${validMonth}.${validYear}`;
+            if (String(validDay).length < 2) {
+            	validDay = '0' + validDay
+            }
+            if (String(validMonth).length < 2) {
+            	validMonth = '0' + validMonth
+            }
             if (nowMonth == userMonth && nowDay == userDay) {
-				alert(`Паспорт действителен\nc ${userDay}.${userMonth}.${getCardValibLastYear}\nдо ${validDay}.${validMonth}.${validYear}( ${validYear}-${validMonth}-${validDay} )\nДЕНЬ РОЖДЕНИЯ СЕГОДНЯ!\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userYear}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
+            	document.getElementById("getCardValibLastYear").innerHTML="Годен с: " + userDay + '.' + userMonth + '.' + getCardValibLastYear;
+            	document.getElementById("getCardValidYear").innerHTML="Годен до: " + validDay + '.' + validMonth +'.' + validYear + '( ' + validYear + '-' + validMonth + '-' + validDay +' )';
+            	document.getElementById("user_age").innerHTML="Возраст: " + userAge;
+				document.getElementById("date_now").innerHTML="Дата сегодня: " + nowDay + '.' + nowMonth  + '.' + nowYear;
+				//alert(`Паспорт действителен\nc ${userDay}.${userMonth}.${getCardValibLastYear}\nдо ${validDay}.${validMonth}.${validYear}( ${validYear}-${validMonth}-${validDay} )\nДЕНЬ РОЖДЕНИЯ СЕГОДНЯ!\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userYear}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
 			} else {
-				alert(`Паспорт действителен\nc ${userDay}.${userMonth}.${getCardValibLastYear}\nдо ${validDay}.${validMonth}.${validYear}( ${validYear}-${validMonth}-${validDay} )\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userYear}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
+				document.getElementById("getCardValibLastYear").innerHTML="Годен с: " + userDay + '.' + userMonth + '.' + getCardValibLastYear;
+            	document.getElementById("getCardValidYear").innerHTML="Годен до: " + validDay + '.' + validMonth +'.' + validYear + '( ' + validYear + '-' + validMonth + '-' + validDay +' )';
+            	document.getElementById("user_age").innerHTML="Возраст: " + userAge;
+				document.getElementById("date_now").innerHTML="Дата сегодня: " + nowDay + '.' + nowMonth  + '.' + nowYear;
+				// alert(`Паспорт действителен\nc ${userDay}.${userMonth}.${getCardValibLastYear}\nдо ${validDay}.${validMonth}.${validYear}( ${validYear}-${validMonth}-${validDay} )\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userYear}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
 			}
 		} else if (userAge >= 45 && userAge < 300) {
 			let getCardValibLastYear = 45 + parseInt(userYear);
 			if (nowMonth == userMonth && nowDay == userDay) {
-				alert(`Паспорт бессрочный.\nНо не раньше: ${userDay}.${userMonth}.${getCardValibLastYear}\nДЕНЬ РОЖДЕНИЯ СЕГОДНЯ!\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userMonth}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
+				document.getElementById("getCardValibLastYear").innerHTML="Годен с: " + userDay + '.' + userMonth + '.' + getCardValibLastYear;
+            	
+            	document.getElementById("user_age").innerHTML="Возраст: " + userAge;
+				document.getElementById("date_now").innerHTML="Дата сегодня: " + nowDay + '.' + nowMonth  + '.' + nowYear;
+				//alert(`Паспорт бессрочный.\nНо не раньше: ${userDay}.${userMonth}.${getCardValibLastYear}\nДЕНЬ РОЖДЕНИЯ СЕГОДНЯ!\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userMonth}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
 			} else {
-				alert(`Паспорт бессрочный.\nНо не раньше: ${userDay}.${userMonth}.${getCardValibLastYear}\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userMonth}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
+				document.getElementById("getCardValibLastYear").innerHTML="Годен с: " + userDay + '.' + userMonth + '.' + getCardValibLastYear;
+            	document.getElementById("getCardValidYear").innerHTML="Годен до: Бессрочный ( 2099-01-01 )";
+            	document.getElementById("user_age").innerHTML="Возраст: " + userAge;
+				document.getElementById("date_now").innerHTML="Дата сегодня: " + nowDay + '.' + nowMonth  + '.' + nowYear;
+				//alert(`Паспорт бессрочный.\nНо не раньше: ${userDay}.${userMonth}.${getCardValibLastYear}\n\nВозраст: ${userAge}\nДата рождения: ${userDay}.${userMonth}.${userMonth}\nДата сегодня: ${nowDay}.${nowMonth}.${nowYear}`);
 			}
 		}
 	}
